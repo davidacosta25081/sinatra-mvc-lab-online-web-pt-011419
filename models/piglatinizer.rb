f691b804961384bf073f5398dbbc7bff38dad80c
David Acosta <davidacosta25081@yahoo.com>
2019-05-20 23:25:11 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,37 +1,35 @@
 class PigLatinizer
 
-  def piglatinize(input_str)
-    input_str.split(" ").length == 1 ? piglatinize_word(input_str) : piglatinize_sentence(input_str)
-  end
+  def piglatinize(word)
+    alphabet = ("A".."z").to_a
+    vowels = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"]
+    consonants = alphabet - vowels
+    unusable = ["i", "a", "an", "in", "on", "and"]
+    word.gsub!(/[^a-z0-9\s]/i, '')
+    word = word.split("")
+    # ["k", "i", "t", "t", "e", "n"]
 
-  private
-
-  def consonant?(char)
-    !char.match(/[aAeEiIoOuU]/)
-  end
-
-  def piglatinize_word(word)
-    # word starts with vowel
-    if !consonant?(word[0])
-      word = word + "w"
-    # word starts with 3 consonants
-    elsif consonant?(word[0]) && consonant?(word[1]) && consonant?(word[2])
-      word = word.slice(3..-1) + word.slice(0,3)
-    # word starts with 2 consonants
-    elsif consonant?(word[0]) && consonant?(word[1])
-      word = word.slice(2..-1) + word.slice(0,2)
-    # word starts with 1 consonant
+    if unusable.include?(word.join) || word.size <= 1
+      word.join
+    elsif vowels.include?(word[0]) && word.size > 1
+      word.join + "ay"
+    elsif consonants.include?(word[0]) && consonants.include?(word[1]) && consonants.include?(word[2])
+      word = word.rotate.rotate.rotate << "ay"
+      word.join
+    elsif consonants.include?(word[0] && word[1])
+      word.rotate.rotate.join + "ay"
     else
-      word = word.slice(1..-1) + word.slice(0)
+      word.rotate.join + "ay"
     end
-    word << "ay"
+
+end
+
+  def to_pig_latin(words)
+    result = words.split(" ").map do |word|
+      piglatinize(word)
+    end
+    result.join(" ")
   end
-
-  def piglatinize_sentence(sentence)
-    sentence.split.collect { |word| piglatinize_word(word) }.join(" ")
-  end
-
-
 
 
 end
